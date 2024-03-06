b657948dacf524d16a23a2c763e8c2c2b88a00eb
Robin A. Meade <meader@hawaii.edu>
2024-03-06 07:57:34 -1000

    sync

--- scripts/multiple-files-demo/Main.java
+++ scripts/multiple-files-demo/Main.java
@@ -3,8 +3,14 @@ import model.Person;
 
 public class Main {
 
-    public static void main(String... args) {
-        Person p = new Person(args[0]);
-        System.out.println("Hello " + p.getName());
+  public static void main(String... args) {
+    String name;
+    if (args.length > 0) {
+      name = args[0];
+    } else {
+      name = "World";
     }
+    Person p = new Person(name);
+    System.out.println("Hello " + p.getName());
+  }
 }

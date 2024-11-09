680c15a7b8b907948430a2a742bccf8093844323
Robin A. Meade <robin.a.meade@gmail.com>
2024-11-08 19:51:50 -1000

    jlist take arg

--- scripts/jlist.java
+++ scripts/jlist.java
@@ -2,20 +2,26 @@ import java.io.File;
 
 public class jlist {
     public static void main(String[] args) {
-        System.out.println("jlist 0.1.2 - Listing of /");
+        System.out.println("jlist 0.1.3 - Take an arg");
         // Get the current working directory
-        String pwd = System.getProperty("user.dir");
-        System.out.println("Current working directory: " + pwd);
+        String userDir = System.getProperty("user.dir");
 
-        // List the files in the current working directory
-        //File dir = new File(pwd);
-        File dir = new File("/");
+        // Use the provided directory argument if available, otherwise use the current working directory
+        String directory = (args.length > 0) ? args[0] : userDir;
+        System.out.println("Directory: " + directory);
+
+        // List the files in the specified directory
+        File dir = new File(directory);
         File[] filesList = dir.listFiles();
 
         if (filesList != null) {
-            System.out.println("Files in the current working directory:");
+            System.out.println("Files in the specified directory:");
             for (File file : filesList) {
-                System.out.println(file.getName());
+                if (file.isDirectory()) {
+                    System.out.println(file.getName() + "/");
+                } else {
+                    System.out.println(file.getName());
+                }
             }
         } else {
             System.out.println("The directory is empty or an error occurred.");
